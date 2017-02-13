1df925e18a89e5247902533d4500b4f9f97dab8f
John Paraskevopoulos <io.paraskev@gmail.com>
2017-02-13 10:14:28 +0200

    Add cli options

--- fcrypter/cli.py
+++ fcrypter/cli.py
@@ -1,10 +1,20 @@
 # -*- coding: utf-8 -*-
 
 import click
+from fcrypter import get_supported_ciphers
+
+
+supported_ciphers = get_supported_ciphers()
+cipher_names = [y.name for y in supported_ciphers]
 
 
 @click.command()
-def main(args=None):
+@click.option("--cipher", type=click.Choice(cipher_names),
+              help="Cipher mode used for encryption")
+@click.option("--mode", type=click.Choice(["encrypt", "e", "decrypt", "d"]))
+@click.option("--file-in", prompt=True)
+@click.option("--file-out", prompt=True)
+def main(cipher, mode, file_in, file_out):
     """Console script for fcrypter"""
     click.echo("Replace this message by putting your code into "
                "fcrypter.cli.main")
--- fcrypter/fcrypter.py
+++ fcrypter/fcrypter.py
@@ -1,1 +1,23 @@
 # -*- coding: utf-8 -*-
+from collections import namedtuple
+import aes
+
+
+supported_ciphers = namedtuple("ciphers", ("name", "callclass"))
+
+
+def get_aes_supported_modes():
+    """
+    Returns a named tuple with supported AES cipher modes and their class
+    name callback
+    """
+    return supported_ciphers(name="AES-EAX", callclass=aes.AESCryptorEAX)
+
+
+def get_supported_ciphers():
+    """
+    Returns a list of named tuples with supported cipher name and callback
+    class name
+    :rtype: tuple of namedtuples
+    """
+    return sum((tuple([x]) for x in (get_aes_supported_modes(),)), tuple())
