f1e0e180b8832cf6f819e981bf4364a8a5a26df5
John Paraskevopoulos <io.paraskev@gmail.com>
2017-02-13 10:15:15 +0200

    Update requirements

--- setup.py
+++ setup.py
@@ -11,11 +11,12 @@ with open('HISTORY.rst') as history_file:
 
 requirements = [
     'Click>=6.0',
-    # TODO: put package requirements here
+    'pycryptodome',
 ]
 
 test_requirements = [
-    # TODO: put package test requirements here
+    'Click>=6.0',
+    'pycryptodome',
 ]
 
 setup(
