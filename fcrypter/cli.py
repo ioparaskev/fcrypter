0104843f67380b0230479a81070e3992a50b4269
John Paraskevopoulos <io.paraskev@gmail.com>
2017-02-16 09:08:46 +0200

    Add key padding when encr key size not supported
    If decryption key size is not supported or encr key size >
    max(supported), raise error

--- fcrypter/cli.py
+++ fcrypter/cli.py
@@ -1,24 +1,37 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 import click
-from fcrypter.fcrypter import get_supported_ciphers
+from fcrypter.fcrypter import get_supported_ciphers, pad_key
 
 
 supported_ciphers = get_supported_ciphers()
-cipher_names = [y.name for y in supported_ciphers]
+cipher_names = supported_ciphers.keys()
 
 
 @click.command()
 @click.option("--cipher", type=click.Choice(cipher_names),
-              help="Cipher mode used for encryption")
-@click.option("--mode", type=click.Choice(["encrypt", "e", "decrypt", "d"]))
+              help="Cipher mode used for encryption", required=True)
+@click.option('--encrypt', 'mode', flag_value='e',
+              default=True)
+@click.option('--decrypt', 'mode', flag_value='d')
 @click.option("--file-in", prompt=True)
 @click.option("--file-out", prompt=True)
-def main(cipher, mode, file_in, file_out):
+@click.option("--password", prompt=True, hide_input=True)
+def main(cipher, mode, file_in, file_out, password):
     """Console script for fcrypter"""
-    click.echo("Replace this message by putting your code into "
-               "fcrypter.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+    ciph = supported_ciphers[cipher]()
+    modes = dict(e='encrypt_file', d='decrypt_file')
+
+    if len(password) not in ciph.supported_sizes:
+        # we cannot support random padding when decrypting
+        if mode == "d" or len(password) > max(ciph.supported_sizes):
+            raise RuntimeError("Wrong key size given!\nMust be size {}".format(
+                " or ".join([str(x) for x in ciph.supported_sizes])))
+        # pad the key with extra chars if key size is not supported
+        password = pad_key(password, ciph)
+        print("Padded key\nYour password is: {p}".format(p=password))
+
+    getattr(ciph, modes[mode])(bytes(password), file_in, file_out)
 
 
 if __name__ == "__main__":
--- fcrypter/fcrypter.py
+++ fcrypter/fcrypter.py
@@ -1,10 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
-from collections import namedtuple
+import string
 from fcrypter import aes
 
+all_chars = string.ascii_letters + string.punctuation
 
-supported_ciphers = namedtuple("ciphers", ("name", "callclass"))
+
+def pad_key(key, cipher):
+    """
+    Adds extra characters to a key to reach the maximum supported key length
+    :param key: encryption key
+    :type cipher: Cryptor
+    :return: str
+    """
+    return "{}{}".format(key, cipher.generate_random_password(
+        size=max(cipher.supported_sizes) - len(key), chars=all_chars))
 
 
 def get_aes_supported_modes():
@@ -12,13 +22,13 @@ def get_aes_supported_modes():
     Returns a named tuple with supported AES cipher modes and their class
     name callback
     """
-    return supported_ciphers(name="AES-EAX", callclass=aes.AESCryptorEAX)
+    return {'AES-EAX': aes.AESCryptorEAX}
 
 
 def get_supported_ciphers():
     """
     Returns a list of named tuples with supported cipher name and callback
     class name
-    :rtype: tuple of namedtuples
+    :rtype: dict
     """
-    return sum((tuple([x]) for x in (get_aes_supported_modes(),)), tuple())
+    return get_aes_supported_modes()
